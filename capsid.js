bc44af14c9bd7169fdfe7a853df19309864026d7
dnanto <dnegron2@gmu.edu>
2020-06-15 12:50:52 -0400

    rotation to place adjacent 5/6-gon to another one...

--- capsid.js
+++ capsid.js
@@ -1,8 +1,15 @@
+function deg2rad(x) {
+    return x * Math.PI / 180;
+}
+
 function draw_regular_polygon(ctx, n, x, y, r, t = 0) {
     ctx.beginPath();
+    // encircle
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
     // draw R'
     ctx.lineTo(x, y);
     // draw edges
+    var coor = [0, 0];
     for (var i = 0; i <= n; i++) {
         ctx.lineTo(
             x + r * Math.cos(t + i * 2 * Math.PI / n),
@@ -10,6 +17,7 @@ function draw_regular_polygon(ctx, n, x, y, r, t = 0) {
         );
     }
     // draw R
+    n *= 2;
     for (var i = 0; i <= n; i++) {
         ctx.moveTo(x, y);
         ctx.lineTo(
@@ -25,13 +33,14 @@ window.onload = function () {
     var ctx = c.getContext("2d");
 
     var r = 100;
+    var t = deg2rad(0);
     var x = c.width / 2, y = c.height / 2;
-    // encircle
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.stroke();
-    // 6-gon
-    draw_regular_polygon(ctx, 6, x, y, r);
-    // 5-gon with edge equivalent to 6-gon
-    draw_regular_polygon(ctx, 5, x, y, r * Math.sin(2 * Math.PI / 6));
+
+    // rotate 5 -> 6
+    draw_regular_polygon(ctx, 5, x, y, r, t);
+    draw_regular_polygon(ctx, 6, x, y, 2 * r * Math.sin(Math.PI / 5), t + deg2rad(6));
+
+    // rotate 6 -> 5
+    // draw_regular_polygon(ctx, 6, x, y, 2 * r * Math.sin(Math.PI / 5));
+    // draw_regular_polygon(ctx, 5, x, y, r, t + deg2rad(30));
 }
