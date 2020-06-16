bcc39a951a6c45f2086ea3711bd1d091e773b675
dnanto <dnegron2@gmu.edu>
2020-06-15 22:47:04 -0400

    set shared edge

--- capsid.js
+++ capsid.js
@@ -32,26 +32,19 @@ window.onload = function () {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
 
-    var R5 = 100;
-    var t = 250;
-    var x = c.width / 2, y = c.height / 2;
+    
+    var x = c.width / 4, y = c.height / 4, t = 10;
 
-    // rotate 5 -> 6
+    // 5->6-gon
+    var R5 = 50;
     draw_regular_polygon(ctx, 5, x, y, R5, deg2rad(t));
 
     var R6 = 2 * R5 * Math.sin(Math.PI / 5);
     // note: R6 = edge length
     // calc: 5-gon + 6-gon inradii
     var r5r6 = R6 / (2 * Math.tan(Math.PI / 5)) + R6 * Math.cos(Math.PI / 6);
-    draw_regular_polygon(
-        ctx, 6,
-        x + r5r6 * Math.sin(deg2rad(54 - t)),
-        y + r5r6 * Math.cos(deg2rad(54 - t)),
-        R6,
-        deg2rad(6+t)
-    );
-
-    // rotate 6 -> 5
-    // draw_regular_polygon(ctx, 6, x, y, 2 * r * Math.sin(Math.PI / 5));
-    // draw_regular_polygon(ctx, 5, x, y, r, t + deg2rad(30));
+    var f = 72 * 0;
+    x += r5r6 * Math.sin(deg2rad(54 - t + f));
+    y += r5r6 * Math.cos(deg2rad(54 - t + f));
+    draw_regular_polygon(ctx, 6, x, y, R6, deg2rad(6 + t - f));
 }
