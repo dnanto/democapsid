433cb9c8542deb64489206bd831a18c6e2a58e99
dnanto <dnegron2@gmu.edu>
2020-06-17 21:47:19 -0400

    Caspar-Klug Theory implementation...

--- capsid.js
+++ capsid.js
@@ -43,68 +43,68 @@ function draw_regular_polygon(ctx, n, x, y, R, r, t = 0) {
 }
 
 window.onload = function () {
-    var R5 = 100;
+    var F = 1, h = 5, k = 0;
+    var t = 0;
+    var dextro = 3, laevo = -1;
+    var handedness = laevo;
+
+    var R5 = 75;
     var R6 = 2 * R5 * Math.sin(Math.PI / 5);
     var r5 = R6 / (2 * Math.tan(Math.PI / 5));
     var r6 = R6 * Math.cos(Math.PI / 6);
 
-    // 5 -> 5
-    {
-        var c = document.getElementById("canvas_5-5");
-        var ctx = c.getContext("2d");
-        var x1 = c.width / 2, y1 = c.height / 2;
-        draw_regular_polygon(ctx, 5, x1, y1, R5, r5);
-        var x2 = x1 + 2 * r5 * Math.cos(deg2rad(36));
-        var y2 = y1 + 2 * r5 * Math.sin(deg2rad(36));
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        draw_regular_polygon(ctx, 5, x2, y2, R5, r5, deg2rad(36));
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    var x1 = R5, y1 = R5;
+    draw_regular_polygon(ctx, 5, x1, y1, R5, r5, deg2rad(t));
+
+    var d = r5 + r6, a1 = 36, a2 = 6;
+    for (let f = 0; f < F; f++) {
+
+        for (let i = 0; i < h - ((f == F - 1) * (k == 0)); i++) {
+            x2 = x1 + d * Math.cos(deg2rad(a1 + t));
+            y2 = y1 + d * Math.sin(deg2rad(a1 + t));
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            draw_regular_polygon(ctx, 6, x2, y2, R6, r6, deg2rad(a2 + t));
+            x1 = x2, y1 = y2;
+            d = 2 * r6;
+        }
+
+        a1 = 30, a2 = 6;
+        for (let j = 0; j < k - (f == F - 1); j++) {
+            x2 = x1 + d * Math.cos(deg2rad(handedness * a1 + a2 + t));
+            y2 = y1 + d * Math.sin(deg2rad(handedness * a1 + a2 + t));
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            draw_regular_polygon(ctx, 6, x2, y2, R6, r6, deg2rad(a2 + t));
+            x1 = x2, y1 = y2;
+        }
+        a1 = 36, a2 = 6;
     }
 
-    // 5 -> 6
-    {
-        var c = document.getElementById("canvas_5-6");
-        var ctx = c.getContext("2d");
-        var x1 = c.width / 2, y1 = c.height / 2;
-        draw_regular_polygon(ctx, 5, x1, y1, R5, r5);
-        var x2 = x1 + (r5 + r6) * Math.cos(deg2rad(36));
-        var y2 = y1 + (r5 + r6) * Math.sin(deg2rad(36));
+    d = r5 + r6;
+    if (k > 0) {
+        a1 = 30;
+        x2 = x1 + d * Math.cos(deg2rad(handedness * 30 + 6 + t));
+        y2 = y1 + d * Math.sin(deg2rad(handedness * 30 + 6 + t));
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
         ctx.stroke();
-        draw_regular_polygon(ctx, 6, x2, y2, R6, r6, deg2rad(6));
-    }
-
-    // 6 -> 5
-    {
-        var c = document.getElementById("canvas_6-5");
-        var ctx = c.getContext("2d");
-        var x1 = c.width / 2, y1 = c.height / 2;
-        draw_regular_polygon(ctx, 6, x1, y1, R6, r6);
-        var x2 = x1 + (r5 + r6) * Math.cos(deg2rad(30));
-        var y2 = y1 + (r5 + r6) * Math.sin(deg2rad(30));
+        draw_regular_polygon(ctx, 5, x2, y2, R5, r5, deg2rad((handedness == dextro) ? 24 : -24 + t));
+        x1 = x2, y1 = y2;
+    } else {
+        x2 = x1 + d * Math.cos(deg2rad(36 + t));
+        y2 = y1 + d * Math.sin(deg2rad(36 + t));
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
         ctx.stroke();
-        draw_regular_polygon(ctx, 5, x2, y2, R5, r5, deg2rad(30));
-    }
-
-    // 6 -> 6
-    {
-        var c = document.getElementById("canvas_6-6");
-        var ctx = c.getContext("2d");
-        var x1 = c.width / 2, y1 = c.height / 2;
-        draw_regular_polygon(ctx, 6, x1, y1, R6, r6);
-        var x2 = x1 + 2 * r6 * Math.cos(deg2rad(30));
-        var y2 = y1 + 2 * r6 * Math.sin(deg2rad(30));
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        draw_regular_polygon(ctx, 6, x2, y2, R6, r6, deg2rad(0));
+        draw_regular_polygon(ctx, 5, x2, y2, R5, r5, deg2rad(36 + t));
     }
 }
