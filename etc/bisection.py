ee357bee21414b0ddef4cbf3d6f418c64ba78ac4
dnanto <dnegron2@gmu.edu>
2022-05-16 12:46:35 -0400

    Update bisection.py

--- etc/bisection.py
+++ etc/bisection.py
@@ -25,14 +25,14 @@ def brackets(f, a, b, iter):
             yield a + (i - 1) * frac, x
             prev = curr
 
-ux, vx, cx = (0.75593, -0.42258, 0.475)
-uy, vy, cy = (-0.65465, -0.48795, 0.72689)
-uz, vz, cz = (0, -0.76376, 0.32095)
-r = 1.64545
+ux, vx, cx = (0.7559289460184544, -0.42257712736425823, 62.50000000000001)
+uy, vy, cy = (-0.6546536707079771, -0.48795003647426666, 116.77154487033951)
+uz, vz, cz = (1.312742527237846e-16, -0.7637626158259734, 152.85593669469102)
+r = 216.50635094610973
 fx = lambda t: r * ux * math.cos(t) + r * vx * math.sin(t) + cx
 fy = lambda t: r * uy * math.cos(t) + r * vy * math.sin(t) + cy
 fz = lambda t: r * uz * math.cos(t) + r * vz * math.sin(t) + cz
-f = lambda t: 0.872673589 - (math.pow(fx(t), 2) + math.pow(fz(t), 2))
+f = lambda t: r * r - (math.pow(fx(t), 2) + math.pow(fz(t), 2))
 
 for a, b in brackets(f, 0, 2 * 3.14, 10000):
     t = bisection(f, a, b, 0.001, 10000)
