ab341ba3c3d6a5a89aa63c893a1d8ffdd33c2c76
dnanto <dnegron2@gmu.edu>
2026-07-01 12:55:53 -0400

    graph object fixes/improvements

--- js/sketch.js
+++ js/sketch.js
@@ -2,69 +2,76 @@ class Graph {
     constructor(hasher = (e) => e) {
         this.nodes = new Map();
         this.edges = new Map();
+        this.neigh = new Map();
         this.hasher = hasher;
     }
 
-    add_node(node) {
-        this.nodes.set(this.hasher(node), node);
+    add_node(key, val) {
+        this.nodes.set(key, val);
+        this.neigh.getOrInsert(key, []);
         return this;
     }
 
+    edgeify(src, tar) {
+        const edge = [src, tar];
+        const keys = edge.map((e) => this.hasher(e));
+        return keys[0] < keys[1] ? [keys, edge] : [keys, edge].map((e) => e.toReversed());
+    }
+
     add_edge(node1, node2) {
-        [node1, node2].forEach((e) => {
-            this.add_node(e);
+        const [keys, edge] = this.edgeify(node1, node2);
+        [0, 1].forEach((i) => {
+            this.add_node(keys[i], edge[i]);
+            this.neigh.getOrInsert(keys[i]).push(keys[(i + 1) % 2]);
         });
-        this.neighbors(node1).push(node2);
-        this.neighbors(node2).push(node1);
+        this.edges.set(keys.join("-"), edge);
         return this;
     }
 
     add_edges(edges) {
-        edges.forEach((e) => {
-            this.add_edge(e[0], e[1]);
-        });
+        edges.forEach((e) => this.add_edge(e[0], e[1]));
         return this;
     }
 
     has_edge(node1, node2) {
-        const target = this.hasher(node2);
-        return this.neighbors(node1).find((e) => this.hasher(e) === target) !== undefined;
+        return this.edges.get(this.sort_edge(node1, node2)[1].join("-"));
     }
 
-    neighbors(node) {
-        // not the best...
-        return this.edges.getOrInsert(this.hasher(node), []);
+    neighbors(key) {
+        return this.neigh.get(key) ?? [];
     }
 
-    dfs(src, tar, dir = 0, path = new Set()) {
+    polygonize(src, tar, path = new Set()) {
         // check cycle
-        const src_key = this.hasher(src);
-        if (path.has(src_key)) {
+        const [src_key, tar_key] = [src, tar].map((e) => this.hasher(e));
+        if (path.values().next().value == src_key) {
             return [Array.from(path).map((e) => this.nodes.get(e)), true];
         }
         // build path
         path.add(src_key);
         // get neighbors, excluding source node
-        const tar_neighbors = this.neighbors(tar).filter((e) => this.hasher(e) !== this.hasher(src));
+        const tar_neighbors = this.neighbors(tar_key)
+            .filter((e) => e !== src_key)
+            .map((e) => this.nodes.get(e));
+        // check if neighbors exist
         if (!tar_neighbors.length) {
             return [Array.from(path).map((e) => this.nodes.get(e)), false];
         }
         // get next node with minumum angle: src - tar - next
         const tar_to_src = src.subtract(tar);
-        const cmp = dir ? (a, b) => a < b : (a, b) => a > b;
         const idx = tar_neighbors
             .map((e) => tar_to_src.getDirectedAngle(e.subtract(tar)))
             .reduce((acc, val, idx, arr) => {
-                /****/ if (cmp(arr[acc], 0) && cmp(0, val)) {
+                /****/ if (arr[acc] < 0 && 0 < val) {
                     return idx;
-                } else if (cmp(val, 0) && cmp(0, arr[acc])) {
+                } else if (val < 0 && 0 < arr[acc]) {
                     return acc;
                 } else {
-                    return cmp(arr[acc], val) ? acc : idx;
+                    return arr[acc] < val ? acc : idx;
                 }
             }, 0);
         // recurse
-        return this.dfs(tar, tar_neighbors[idx], dir, path);
+        return this.polygonize(tar, tar_neighbors[idx], path);
     }
 }
 
@@ -116,31 +123,14 @@ function draw(paper, s, R = 50) {
     [g, g1, g2, g3].forEach((e) => e.remove());
 
     const G = new Graph((hasher = hash_point)).add_edges(lattice.children.flatMap((e) => e.children).map((e) => [e.segments[0].point, e.segments[1].point]));
-
-    const nodes = Array.from(G.nodes.values());
-    const src = nodes[Math.floor(Math.random() * nodes.length)];
-    const neighbors = Array.from(G.neighbors(src));
-    const tar = neighbors[Math.floor(Math.random() * neighbors.length)];
-    const paths = [0, 1]
-        .map((e) => {
-            return G.dfs(src, tar, e);
-        })
-        .filter((e) => e[1]);
-
-    let path;
-    /****/ if (paths.length === 0) {
-        path = [];
-    } else if (paths.length === 1) {
-        path = paths[0][0];
-    } else if (paths.length === 2) {
-        path = paths[0][0].length < paths[1][0].length ? paths[0][0] : paths[1][0];
-    }
-
-    const p = new paper.Group({
+    const edges = Array.from(G.edges);
+    const edge = edges[Math.floor(Math.random() * edges.length)][1];
+    const path = G.polygonize(edge[0], edge[1]);
+    new paper.Group({
         children: [
-            new paper.Path.Circle({ center: src, radius: 2, fillColor: "green" }),
-            new paper.Path.Circle({ center: tar, radius: 2, fillColor: "red" }),
-            new paper.Path({ segments: path, strokeColor: "blue", strokeWidth: 2, closed: true }),
+            new paper.Path.Circle({ center: edge[0], radius: 2, fillColor: "green" }),
+            new paper.Path.Circle({ center: edge[1], radius: 2, fillColor: "red" }),
+            new paper.Path({ segments: path[0], strokeColor: path[1] ? "blue" : "orange", strokeWidth: 2, closed: path[1] }),
         ],
     });
 }
